7bfe8567f85686f09d0fb4f5ed1e6cff09eb9fbd
JoonkiKim <qkjk1508@gmail.com>
2024-11-26 15:45:51 +0900

    분기배포

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
   trailingSlash: true,
+  generateBuildId: () => "codecamp-deploy-project",
+
+
+  // 아래의 주소만 out폴더에 만들어줘 (getServerSideProps가 있는 페이지는 제외함) 
+  exportPathMap: () => 
+
+ ({
+  "/" : {page:"/"},
+  "/boards" : {page: "boards"},
+  // "/404" : {page: "/404"}
+})  
 };
 
 export default nextConfig;
